ca3a24e924c4c0241528be71422a6fa0973e4f79
Zack Peterson <mail@zack.wtf>
2019-04-13 20:34:20 -0600

    Update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Hello from "component/Hello";
-import "normalize.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<Hello />, document.getElementById("root"));
